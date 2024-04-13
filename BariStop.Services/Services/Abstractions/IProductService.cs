dde09951c5d1bf61031a8c522621794a6419679a
Ekaterina18s <ekaterinaelzo18@gmail.com>
2024-04-13 12:47:31 +0300

    added services + DTOs

---
+++ BariStop.Services/Services/Abstractions/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using BariStop.Data.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BariStop.Services.Services.Abstractions
+{
+    public class IProductService
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+    }
+}
