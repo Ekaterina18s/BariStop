1b2fb31c6ff95816fb8d8d86ca08c2eaab3e8ca7
Ekaterina18s <ekaterinaelzo18@gmail.com>
2024-04-13 11:41:47 +0300

    Data Acess Layer + greshka

--- BariStop.Data/Repositories/Abstractions/IRepository.cs
+++ BariStop.Data/Repositories/Abstractions/IRepository.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using BariStop.Data.Data.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BariStop.Data.Repositories.Abstractions
 {
-    internal class IRepository
+    public interface IRepository<T> where T : BaseEntity
     {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> predicate);
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteByIdAsync(int id);
+
+
     }
 }
